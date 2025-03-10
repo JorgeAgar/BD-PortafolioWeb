4b5c6570143fa8d101f6ce4b1d248fc13e796174
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-03-10 11:32:51 -0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("formulario");
+const name = document.getElementById("nombre");
+const code = document.getElementById("codigo");
+const email = document.getElementById("email");
+const github_link = document.getElementById("github");
+const photo = document.getElementById("fotoURL");
+
+// form.addEventListener("submit", () => {
+//     const estudiante = {
+//         name,
+//         code,
+//         email,
+//         github_link,
+//         photo
+//     };
+
+//     createStudent(estudiante);
+// });
