712358afb2aca8eeebfbe5c32023af2f0d5dbcb0
Jorge <ogeandes1974@gmail.com>
2025-03-11 15:09:46 -0500

    progreso en anadir

--- js/form.js
+++ js/form.js
@@ -2,17 +2,30 @@ const form = document.getElementById("formulario");
 const name = document.getElementById("nombre");
 const code = document.getElementById("codigo");
 const email = document.getElementById("email");
+const description = document.getElementById("descripcion");
 const github_link = document.getElementById("github");
 const photo = document.getElementById("fotoURL");
 
-// form.addEventListener("submit", () => {
-//     const estudiante = {
-//         name,
-//         code,
-//         email,
-//         github_link,
-//         photo
-//     };
+document.getElementById("crearStudent").addEventListener("click", () => {
+    if(!form.checkValidity()){
+        return;
+    }
+    
+    const estudiante = {
+        code: code.value,
+        name: name.value,
+        email: email.value,
+        photo: photo.value,
+        github_link: github_link.value,
+        description: description.value,
+    };
 
-//     createStudent(estudiante);
-// });
+    try {
+        console.log(estudiante);
+        let response = api.createStudent(estudiante);
+        console.log(response);
+        // alert("Estudiante creado exitosamente");
+    } catch (error) {
+        console.error(error);
+    }
+});
