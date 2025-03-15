ee91e11e9aaf15c5b80abb16c467b5cef53d384e
Jorge <ogeandes1974@gmail.com>
2025-03-15 17:41:00 -0500

    funciona estrellas de rating y visual de detalles

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             clone.querySelector('.student-id').textContent = `ID: ${student.code}`;
             clone.querySelector('.student-email').textContent = student.email;
             clone.querySelector('.student-image').src = student.photo;
-            clone.querySelector('.github-link').href = `https://github.com/${student.github_link}`;
+            clone.querySelector('.github-link').href = student.github_link;
             clone.querySelector('.edit').onclick = () => {
                 window.location.href = 'edit.html?code=' + student.code;
             };
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 detailsClone.querySelector('.details-name').textContent = student.name;
                 detailsClone.querySelector('.details-code').textContent = student.code;
                 detailsClone.querySelector('.details-email').textContent = student.email;
-                detailsClone.querySelector('.details-github').href = `https://github.com/${student.github_link}`;
+                detailsClone.querySelector('.details-github').href = student.github_link;
                 detailsClone.querySelector('.details-student-description').textContent = student.description;
                 detailsClone.querySelector('.details-back').onclick = () => {
                     const viewInstance = document.body.querySelector('.details-view');
@@ -50,14 +50,23 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 const tech_template = document.getElementById('technologyTemplate');
+const star_template = document.getElementById('starTemplate');
+
 async function fillTechnologies(ulist, student_code){
     const technologies = await api.getStudentTechnologies(student_code);
 
     technologies.forEach(technology => {
         const clone = tech_template.content.cloneNode(true);
+        let rating = technology.level;
+        for(let i = 0; i < rating; i++){
+            const starList = clone.querySelector('.tech-stars');
+            const starClone = star_template.content.cloneNode(true);
+            console.log(starList);
+            console.log(clone);
+            starList.appendChild(starClone);
+        }
         clone.querySelector('.tech-name').textContent = technology.technology.name;
         clone.querySelector('.tech-img').src = technology.technology.image;
         ulist.appendChild(clone);
-        console.log(clone.querySelector('.tech-stars'));
     });
 }
